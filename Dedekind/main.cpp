5e80942c630463cc9d7c506d6a0a61ada22f8298
bachproef <maxdekoninck@Maxs-MacBook-Pro.local>
2013-11-12 18:43:32 +0100

    Initial Commit

---
+++ Dedekind/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Dedekind
+//
+//  Created by Max Dekoninck on 12/11/13.
+//  Copyright (c) 2013 Daan en Max. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
