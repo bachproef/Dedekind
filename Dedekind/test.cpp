fd6f219c61637e012cd98525cc1b989bc2e37543
bachproef <maxdekoninck@Maxs-MacBook-Pro.local>
2013-11-26 12:10:12 +0100

    l

---
+++ Dedekind/test.cpp
@@ -0,0 +1,15 @@
+//
+//  test.cpp
+//  Dedekind
+//
+//  Created by Max Dekoninck on 17/11/13.
+//  Copyright (c) 2013 Daan en Max. All rights reserved.
+//
+
+#include "test.h"
+
+int main(){
+    
+    std::cout << "JOO";
+    return 0;
+}
---
+++ Dedekind/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  Dedekind
+//
+//  Created by Max Dekoninck on 17/11/13.
+//  Copyright (c) 2013 Daan en Max. All rights reserved.
+//
+
+#ifndef __Dedekind__test__
+#define __Dedekind__test__
+
+#include <iostream>
+
+#endif /* defined(__Dedekind__test__) */
